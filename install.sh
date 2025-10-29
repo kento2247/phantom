814ad3df676d1a8f69abcf705926ac727690cc22
kento2247 <tkento1985@icloud.com>
2025-10-29 12:09:32 +0900

    cuda121→118

--- install.sh
+++ install.sh
@@ -1,59 +1,58 @@
 # ######################## Phantom Env ###############################
 uv venv --python 3.10.0
 source .venv/bin/activate
-pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu118
+uv pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu118
 
 # Install SAM2
 cd submodules/sam2
-pip install -v -e ".[notebooks]"
+uv pip install -v -e ".[notebooks]"
 cd ../..
 
 # Install Hamer
 cd submodules/phantom-hamer
-pip install -e .\[all\]
-pip install -v -e third-party/ViTPose
+uv pip install -e .\[all\]
+uv pip install -v -e third-party/ViTPose
 aria2c -x10 -s10 -k1M https://www.cs.utexas.edu/~pavlakos/hamer/data/hamer_demo_data.tar.gz
 tar --warning=no-unknown-keyword --exclude=".*" -xvf hamer_demo_data.tar.gz
 cd ../..
 
 # Install mmcv
-pip install --index-url https://download.pytorch.org/whl/cu121 torch==2.1.0 torchvision==0.16.0
-pip install mmcv==1.3.9
-pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.1/index.html
-pip install numpy==1.26.4
+uv pip install mmcv==1.3.9
+uv pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
+uv pip install numpy==1.26.4
 
 # Install phantom-robosuite
 cd submodules/phantom-robosuite
-pip install -e .
+uv pip install -e .
 cd ../..
 
 # Install phantom-robomimic
 cd submodules/phantom-robomimic
-pip install -e .
+uv pip install -e .
 cd ../..
 
 # Install additional packages
-pip install joblib mediapy open3d pandas
-pip install transformers==4.42.4
-pip install PyOpenGL==3.1.4
-pip install Rtree
-pip install git+https://github.com/epic-kitchens/epic-kitchens-100-hand-object-bboxes.git
-pip install protobuf==3.20.0
-pip install hydra-core==1.3.2
-pip install omegaconf==2.3.0
+uv pip install joblib mediapy open3d pandas
+uv pip install transformers==4.42.4
+uv pip install PyOpenGL==3.1.4
+uv pip install Rtree
+uv pip install git+https://github.com/epic-kitchens/epic-kitchens-100-hand-object-bboxes.git
+uv pip install protobuf==3.20.0
+uv pip install hydra-core==1.3.2
+uv pip install omegaconf==2.3.0
 
 # Download E2FGVI weights
 cd submodules/phantom-E2FGVI/E2FGVI/release_model/
-pip install gdown
+uv pip install gdown
 gdown --fuzzy https://drive.google.com/file/d/10wGdKSUOie0XmCr8SQ2A2FeDe-mfn5w3/view?usp=sharing
 cd ../..
 
 # Install phantom-E2FGVI
-pip install -e .
+uv pip install -e .
 cd ../..
 
 # Install phantom 
-pip install -e .
+uv pip install -e .
 
 # Download sample data
 cd data/raw
